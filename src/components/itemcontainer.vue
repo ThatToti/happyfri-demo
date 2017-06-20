8f44841ad71af8a2b911dd7ba46d054456441e18
ThatToti <mr.totihuang@gmail.com>
2017-06-20 08:09:42 +0800

    新增配置文件

---
+++ src/components/itemcontainer.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+        <header class="top_tips">
+            <span class="num_tip" v-if="fatherComponent=='home'">{{level}}</span>
+            <span class="num_tip" v-if="fatherComponent=='item'">题目{{itemNum}}</span>
+        </header>
+    </section>
+</template>
+
+<script>
+import {mapState,mapActions}from 'vuex'
+
+export default{
+    name:'itemcontainer',
+    data(){
+
+    },
+    computed:mapState({
+        itemNum:state=>state.itemNum,
+        level:state=>state.level,
+        itemDetail:state=>state.itemDetail
+    }),
+    methods:{
+        
+    }
+}
+</script>
+
+<style>
+</style>
